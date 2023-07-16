7627055930e49a00860fd420141d2b13e336051e
Lukas Bals <mail@lukasbals.at>
2023-07-16 12:28:01 +0200

    Update cursor and add greating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
 "use client";
+import Image from "next/image";
 import styles from "./page.module.css";
-import { Center, Heading, Text } from "boemly";
+import { Box, Button, Center, Heading, Text } from "boemly";
+import { useState } from "react";
 
 export default function Home() {
+  const [isFranziShown, setIsFranziShown] = useState(false);
+
   return (
-    <main>
+    <main className={styles.main}>
       <Center height="xl" flexDir="column" gap="4">
         <Heading size="4xl">Easy Pace</Heading>
         <Text size="mdRegularNormal">
           Radeln macht gemeinsam am meisten Spasssssss
         </Text>
+
+        <Button
+          onClick={() => {
+            setIsFranziShown(true);
+
+            setTimeout(() => {
+              setIsFranziShown(false);
+            }, 1500);
+          }}
+        >
+          Say hello
+        </Button>
       </Center>
+      <Box
+        display={isFranziShown ? "unset" : "none"}
+        position="absolute"
+        top="0"
+        left="0"
+        width="100vw"
+        height="100vh"
+      >
+        <Box position="relative" width="full" height="full">
+          <Image
+            src="/welcome-from-franzi.webp"
+            alt="Franzi weloming our guests"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </Box>
+      </Box>
     </main>
   );
 }
