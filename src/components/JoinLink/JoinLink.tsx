ff53c2c17efc9900598cb3cc95be326279c83c9c
Lukas Bals <mail@lukasbals.at>
2023-07-16 21:50:12 +0200

    Show join link anyways

--- src/components/JoinLink/JoinLink.tsx
+++ src/components/JoinLink/JoinLink.tsx
@@ -5,15 +5,11 @@ import Link from "next/link";
 export const JoinLink = (): JSX.Element => {
   const { data: session } = useSession();
 
-  if (!session) {
-    return (
-      <Link href="https://www.strava.com/clubs/easypace" target="_blank">
-        <Button colorScheme="gray" color="#fc5200">
-          Join us on Strava
-        </Button>
-      </Link>
-    );
-  }
-
-  return <></>;
+  return (
+    <Link href="https://www.strava.com/clubs/easypace" target="_blank">
+      <Button colorScheme="gray" color="#fc5200">
+        Join us on Strava
+      </Button>
+    </Link>
+  );
 };
