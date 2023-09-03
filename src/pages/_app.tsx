64a4633d547ecef7b1e393a08e0c7511b402e06c
Lukas Bals <mail@lukasbals.at>
2023-09-03 23:01:04 +0200

    Add share metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 import Logo from "@/components/Logo";
 import JoinLink from "@/components/JoinLink";
+import Head from "next/head";
 
 export default function MyApp({
   Component,
@@ -20,6 +21,28 @@ export default function MyApp({
 }: AppProps) {
   return (
     <BoemlyThemeProvider>
+      <Head>
+        <meta name="description" content="Best cycling club in se world!" />
+
+        <meta property="og:url" content="https://easypace.at" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Easy Pace" />
+        <meta
+          property="og:description"
+          content="Best cycling club in se world!"
+        />
+        <meta property="og:image" content="/share-image.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="easypace.at" />
+        <meta property="twitter:url" content="https://easypace.at" />
+        <meta name="twitter:title" content="Easy Pace" />
+        <meta
+          name="twitter:description"
+          content="Best cycling club in se world!"
+        />
+        <meta name="twitter:image" content="/share-image.png" />
+      </Head>
       <SessionProvider session={session}>
         <Layout
           header={
